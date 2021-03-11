6c886ab2cd2c1703e85deec799bec49508e135a3
Christian Ordonez <csordonez@hotmail.com>
2021-03-10 23:47:36 -0500

    ETV primera parte

--- lib/pages/etv_page.dart
+++ lib/pages/etv_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
 class ETVPage extends StatefulWidget {
@@ -7,23 +8,29 @@ class ETVPage extends StatefulWidget {
 }
 
 class _ETVPageState extends State<ETVPage> {
-
-  var pesoD = 0.0, tallaD = 0.0, imcD = 0.0, imcR = 0.0;
-
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
-
   final edad = new TextEditingController(text: '');
   final peso = new TextEditingController(text: '');
   final talla = new TextEditingController(text: '');
   final nrocigarillos = new TextEditingController(text: '');
   final nrodepartos = new TextEditingController(text: '');
 
+  var pesoD = 0.0,
+      tallaD = 0.0,
+      imcD = 0.0,
+      imcR = 0.0,
+      edadD = 0,
+      nropartos = 0,
+      nrocigD = 0;
+
+  Color colorE, colorC, colorP;
+  double riskScore;
+  int a = 0, b = 0, c = 0;
+
   TextStyle estilo = TextStyle(fontSize: 26, color: Colors.pink);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
       body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -38,19 +45,37 @@ class _ETVPageState extends State<ETVPage> {
               etiqueta: 'Edad',
               teclado: TextInputType.number,
               icon: FontAwesome5Solid.calendar,
+              color: colorE,
               textcontroller: edad,
+              onChanged: (e) {
+                setState(() {
+                  if (double.parse(edad.text) > 35) {
+                    a = 1;
+                    colorE = Colors.green[100];
+                  } else {
+                    a = 0;
+                    colorE = Colors.pink[100];
+                  }
+                });
+              },
             ),
             TextFieldWidget(
               etiqueta: 'Peso en kg',
               teclado: TextInputType.number,
               icon: FontAwesome5Solid.weight,
               textcontroller: peso,
+              color: Colors.pink[100],
+              onChanged: (e) {
+                calcularIMC();
+                setState(() {});
+              },
             ),
             TextFieldWidget(
               etiqueta: 'Talla en cm',
               teclado: TextInputType.number,
               icon: FontAwesome5Solid.ruler_vertical,
               textcontroller: talla,
+              color: Colors.pink[100],
               onChanged: (e) {
                 calcularIMC();
                 setState(() {});
@@ -61,17 +86,42 @@ class _ETVPageState extends State<ETVPage> {
               teclado: TextInputType.number,
               icon: FontAwesome5Solid.smoking,
               textcontroller: nrocigarillos,
+              color: colorC,
+              onChanged: (c) {
+                setState(() {
+                  if (double.parse(nrocigarillos.text) > 10) {
+                    b = 1;
+                    colorC = Colors.green[100];
+                  } else {
+                    b = 0;
+                    colorC = Colors.pink[100];
+                  }
+                });
+              },
             ),
             TextFieldWidget(
               etiqueta: 'Numero de partos',
               teclado: TextInputType.number,
               icon: FontAwesome5Solid.baby,
               textcontroller: nrodepartos,
+              color: colorP,
+              onChanged: (p) {
+                setState(() {
+                  if (double.parse(nrodepartos.text) > 2) {
+                    c = 1;
+                    colorP = Colors.green[100];
+                  } else {
+                    c = 0;
+                    colorP = Colors.pink[100];
+                  }
+                  riskScoreCalc();
+                });
+              },
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                width: 200,
+                width: 290,
                 height: 55,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
@@ -87,21 +137,24 @@ class _ETVPageState extends State<ETVPage> {
                 ),
               ),
             ),
+            Text(
+              'Risk score es ${riskScoreCalc()}',
+              style: estilo,
+            )
           ]),
     );
   }
-   bool checkNumbers() {
+
+  bool checkNumbers() {
     try {
       pesoD = double.parse(peso.text);
       tallaD = double.parse(talla.text);
       return true;
     } on FormatException catch (e) {
-      _scaffoldKey.currentState.showSnackBar(SnackBar(
-          content: Text(
-              "Rellene todos los campos, por favor\nUse punto en lugar de coma para decimales")));
-
       setState(() {
         imcR = 0.0;
+        pesoD = 0.0;
+        tallaD = 0.0;
       });
       return false;
     }
@@ -115,8 +168,6 @@ class _ETVPageState extends State<ETVPage> {
       imcR = double.parse(imcD.toStringAsFixed(2));
 
       if (imcD > 100) {
-        _scaffoldKey.currentState
-            .showSnackBar(SnackBar(content: Text("La talla debe estar en cm")));
         setState(() {
           imcR = 0.0;
         });
@@ -125,6 +176,10 @@ class _ETVPageState extends State<ETVPage> {
     return imcR;
   }
 
+  int riskScoreCalc() {
+    int totalrisk = a + b + c;
+    return totalrisk;
+  }
 }
 
 class TextFieldWidget extends StatelessWidget {
@@ -132,14 +187,18 @@ class TextFieldWidget extends StatelessWidget {
   final IconData icon;
   final TextInputType teclado;
   final Function onChanged;
+  final Function onSubmitted;
   final TextEditingController textcontroller;
+  Color color = Colors.pink[100];
 
-  const TextFieldWidget(
+  TextFieldWidget(
       {this.etiqueta,
       this.icon,
       this.teclado,
       this.onChanged,
-      this.textcontroller});
+      this.onSubmitted,
+      this.textcontroller,
+      this.color});
 
   @override
   Widget build(BuildContext context) {
@@ -150,10 +209,11 @@ class TextFieldWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        width: 200,
+        width: 290,
         decoration: BoxDecoration(
-            color: Colors.pink[100], borderRadius: BorderRadius.circular(10)),
-        child: TextField(
+            color: this.color, borderRadius: BorderRadius.circular(10)),
+        child: TextFormField(
+          autocorrect: true,
           style: estilo,
           keyboardType: teclado,
           decoration: InputDecoration(
